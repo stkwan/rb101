fb3ec71af9f0dc194e5a8407d4e066f89258f458
Steven Kwan <stevent.kwan@gmail.com>
2021-08-18 15:04:35 -0700

    Ran rubocop on calculator.rb and made corrections

--- lesson_2/calculator.rb
+++ lesson_2/calculator.rb
@@ -35,29 +35,25 @@ prompt("Hello #{name}!")
 
 loop do
   number1 = ''
-  loop do 
+  loop do
     prompt("What's the first number?")
     number1 = Kernel.gets().chomp()
-    
     if valid_number?(number1)
       break
     else
       prompt("Please enter a valid number.")
     end
   end
-  
   number2 = ''
   loop do
     prompt("What's the second number?")
     number2 = Kernel.gets().chomp()
-    
     if valid_number?(number2)
       break
     else
       prompt("Please enter a valid number.")
     end
   end
-  
   operator_prompt = <<-MSG
     What operation would you like to perform?
     1) add
@@ -65,35 +61,28 @@ loop do
     3) multiply
     4) divide
   MSG
-  
   prompt(operator_prompt)
-  
   operator = ''
   loop do
     operator = Kernel.gets().chomp()
-
     if %w(1 2 3 4).include?(operator)
       break
     else
       prompt("Please enter 1, 2, 3, or 4")
     end
   end
-  
   prompt("#{operation_to_message(operator)} the two numbers...")
-      
   result = case operator
-            when '1'
-              number1.to_i() + number2.to_i()
-            when '2'
-              number1.to_i() - number2.to_i()
-            when '3'
-              number1.to_i() * number2.to_i()
-            when '4'
-              number2.to_f() / number2.to_f()
-            end
-  
+           when '1'
+             number1.to_i() + number2.to_i()
+           when '2'
+             number1.to_i() - number2.to_i()
+           when '3'
+             number1.to_i() * number2.to_i()
+           when '4'
+             number2.to_f() / number2.to_f()
+           end
   prompt("The result is #{result}")
-  
   prompt("Would you like to perform another calcuation? (If yes, enter 'Y')")
   another_calc = Kernel.gets().chomp()
   break unless another_calc.downcase.start_with?('y')
