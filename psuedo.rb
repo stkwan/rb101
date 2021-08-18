01d2bf69a30e7b859e94c0384aaa4eb5f324ef61
Steven Kwan <stevent.kwan@gmail.com>
2021-08-17 22:35:23 -0700

    Finished psuedo-code assignment

---
+++ psuedo.rb
@@ -0,0 +1,24 @@
+# 1) a method that returns the sum of two integers
+
+# method will take two integers as arguements
+# retuns integer1 + integer2 using mathmatical addition
+# end
+
+
+# 2) method takes an array of strings and returns a string that is all those strings concatenated
+
+# method takes in an array of strings
+# iniitalize an empty string and assign to variable
+# iterate through the array and shovel each string to the empty string
+# return the variable
+# end
+
+
+# 3) method takes an array of integers and returns new array with every other element
+
+# method accepts an array of integers as an arugument
+# intiialize a new empty array
+# iteratre through the array and select either every odd or every even indexed element in array
+# shovel selected elements into new array
+# retun new array
+
