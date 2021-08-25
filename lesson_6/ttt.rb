de4d09aaa213f8543c2fabc6150ddda441aff0b5
Steven Kwan <stevent.kwan@gmail.com>
2021-08-25 13:21:53 -0700

    Added bonus features: joinor,keep score, ai defense, ai offense, pick 5.

--- lesson_6/ttt.rb
+++ lesson_6/ttt.rb
@@ -4,6 +4,8 @@ COMPUTER_MARKER = "O"
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9],  # rows
                  [1, 4, 7], [2, 5, 8], [3, 6, 9],  # cols
                  [1, 5, 9], [3, 5, 7]]             # diag
+MAX_WINS = 3
+SCORES = { player: 0, computer: 0 }
 
 def prompt(message)
   puts "=> #{message}"
@@ -32,7 +34,7 @@ end
 def initalize_board
   new_board = {}
   (1..9).each { |num| new_board[num] = INITIAL_MARKER }
-  new_board #=> {1: " ", 2: " ", 3: " ", 4: " ", 5: " ",...}
+  new_board #=> {1=>" ", 2=>" ", 3=>" ", 4=>" ", 5=>" ",...9=>" "}
 end
 
 def empty_squares(brd)
@@ -42,7 +44,7 @@ end
 def player_places_peice!(brd) # put a bang b/c it mutates board
   square = ""
   loop do
-    prompt("Choose a square: (#{empty_squares(brd).join(', ')})")
+    prompt("Choose a square: (#{joinor(empty_squares(brd))})")
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt("Sorry, that's not a valid choice.")
@@ -50,9 +52,46 @@ def player_places_peice!(brd) # put a bang b/c it mutates board
   brd[square] = PLAYER_MARKER
 end
 
+def joinor(arr, delimiter=", ", word="or")
+  case arr.size
+  when 0 then ''
+  when 1 then arr[0]
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
+# rubocop:disable Metrics/CyclomaticComplexity,Metrics/MethodLength
 def computer_places_peice!(brd)
-  square = empty_squares(brd).sample
+  square = nil
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd, COMPUTER_MARKER)
+    break if square
+  end
+  if !square
+    WINNING_LINES.each do |line|
+      square = find_at_risk_square(line, brd, PLAYER_MARKER)
+      break if square
+    end
+  end
+  if !square
+    if brd[5] == INITIAL_MARKER
+      square = 5
+    end
+  end
+  if !square
+    square = empty_squares(brd).sample
+  end
   brd[square] = COMPUTER_MARKER
+end
+# rubocop:enable Metrics/CyclomaticComplexity, Metrics/MethodLength
+
+def find_at_risk_square(line, board, marker)
+  if board.values_at(*line).count(marker) == 2
+    board.select { |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
+  end
 end
 
 def board_full?(brd)
@@ -74,6 +113,27 @@ def detect_winner(brd)
   nil
 end
 
+def increment_scores(winner)
+  if winner == "Player"
+    SCORES[:player] += 1
+  elsif winner == "Computer"
+    SCORES[:computer] += 1
+  end
+end
+
+def display_scores
+  prompt("SCORES: Player: #{SCORES[:player]} | Computer: #{SCORES[:computer]}")
+end
+
+def max_wins_reached?
+  SCORES[:player] == MAX_WINS || SCORES[:computer] == MAX_WINS
+end
+
+def reset_scores
+  SCORES[:player] = 0
+  SCORES[:computer] = 0
+end
+
 loop do
   board = initalize_board
   loop do
@@ -85,13 +145,24 @@ loop do
   end
   display_board(board)
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+    prompt "#{detect_winner(board)} won this round!"
+    increment_scores(detect_winner(board))
   else
-    prompt "It's a tie!"
+    prompt "This round is a tie!"
   end
-  prompt("Play again? (y or n)")
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  display_scores
+  if max_wins_reached?
+    prompt("#{detect_winner(board)} wins the game!")
+    prompt("Play again? (y or n)")
+    answer = gets.chomp
+    break unless answer.downcase.start_with?('y')
+  end
+  if max_wins_reached? == false
+    prompt("Press Enter for next round...")
+    continue = gets.chomp
+    next if continue == "/n"
+  end
+  reset_scores if max_wins_reached?
 end
 
-prompt("Thanks for playing Tic-Tac-Toe!")
+prompt("Thanks for playing Tic Tac Toe!")
