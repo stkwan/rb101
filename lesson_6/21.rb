d43ad9a481412f3a43430add20c0992a7f8a38d2
Steven Kwan <stevent.kwan@gmail.com>
2021-08-26 15:14:58 -0700

    rubocop review done

--- lesson_6/21.rb
+++ lesson_6/21.rb
@@ -13,7 +13,7 @@ def initialize_deck
   deck = []
   faces.each do |face|
     suits.each do |suit|
-      deck << "#{face} of #{suit}"  # re-factor code to have each face point to integer?
+      deck << "#{face} of #{suit}" # re-factor code? face point to integer?
     end
   end
   deck
@@ -24,8 +24,8 @@ def shuffle(deck)
 end
 
 def deal_cards(deck)
-  2.times { |n| PLAYER_HAND << deck.pop }
-  2.times { |n| DEALER_HAND << deck.pop }
+  2.times { PLAYER_HAND << deck.pop }
+  2.times { DEALER_HAND << deck.pop }
 end
 
 def get_card_value(card)
@@ -40,26 +40,23 @@ def get_card_value(card)
   end
 end
 
+# rubocop:disable Metrics/MethodLength
 def get_hand_value(hand)
   sum = 0
   hand.each do |card|
     sum += get_card_value(card)
   end
-  
   num_aces_in_hand = 0
   hand.each do |card|
     if card.start_with?("A")
       num_aces_in_hand += 1
     end
   end
-  
-  while sum > 21 &&
-    num_aces_in_hand != 0
+  while sum > 21 && num_aces_in_hand != 0
     num_aces_in_hand -= 1
     sum -= 10
     break if num_aces_in_hand == 0
   end
-  
   sum
 end
 
@@ -69,7 +66,6 @@ def player_option(deck)
     break if busted?(PLAYER_HAND)
     prompt("Would you like to hit or stay? (h/s)")
     answer = gets.chomp
-    
     if answer == 'h'
       PLAYER_HAND << deal_another_card(deck)
       prompt("You got a #{PLAYER_HAND.last} added to your hand.")
@@ -82,34 +78,32 @@ def player_option(deck)
     end
   end
 end
+# rubocop:enable Metrics/MethodLength
 
 def dealer_option(deck)
   prompt("Dealer shows #{DEALER_HAND[0]} and #{DEALER_HAND[1]}.")
-  
   while get_hand_value(DEALER_HAND) < 17
     DEALER_HAND << deal_another_card(deck)
     prompt("Dealer hit and #{DEALER_HAND.last} was added to hand.")
+    prompt("")
     prompt("Dealer current total is #{get_hand_value(DEALER_HAND)}")
     break if busted?(DEALER_HAND)
   end
 end
 
 def busted?(hand)
-  if get_hand_value(hand) > 21
-    return true
-  else
-    return false
-  end
+  get_hand_value(hand) > 21
 end
 
-def deal_another_card(deck) #returns card from current_deck
+def deal_another_card(deck) # returns card from current_deck
   deck.pop
 end
 
+# rubocop:disable Layout/LineLength
 def compare_hands(hand1, hand2)
   prompt("You have #{get_hand_value(hand1)}. Dealer has #{get_hand_value(hand2)}.")
-  if get_hand_value(hand1) > get_hand_value(hand2) && 
-     get_hand_value(hand1) <= 21 && 
+  if get_hand_value(hand1) > get_hand_value(hand2) &&
+     get_hand_value(hand1) <= 21 &&
      get_hand_value(hand2) <= 21
     prompt("YOU WIN!")
   elsif get_hand_value(hand1) == get_hand_value(hand2)
@@ -118,41 +112,32 @@ def compare_hands(hand1, hand2)
     prompt("DEALER WINS!")
   end
 end
+# rubocop:enable Layout/LineLength
 
 loop do
   system "clear"
   current_deck = initialize_deck
   shuffle(current_deck)
   deal_cards(current_deck)
-  
   prompt("Dealer has #{DEALER_HAND[0]} & an unknown card.")
   prompt("You have #{PLAYER_HAND[0]} & #{PLAYER_HAND[1]}.")
   prompt("")
-  
-  
-    player_option(current_deck)
-    
-    if busted?(PLAYER_HAND)
-      prompt("You BUSTED! Dealer wins this round!")
-    end
-    
-    unless busted?(PLAYER_HAND) == true
-      dealer_option(current_deck)
-    end
-    
-    if busted?(DEALER_HAND)
-      prompt("Dealer BUSTED! You win this round!")
-    end
-    
-    if busted?(PLAYER_HAND) == false &&
-      busted?(DEALER_HAND) == false
-      compare_hands(PLAYER_HAND, DEALER_HAND)
-    end
-    
+  player_option(current_deck)
+  if busted?(PLAYER_HAND)
+    prompt("You BUSTED! Dealer wins this round!")
+  end
+  unless busted?(PLAYER_HAND) == true
+    dealer_option(current_deck)
+  end
+  if busted?(DEALER_HAND)
+    prompt("Dealer BUSTED! You win this round!")
+  end
+  if busted?(PLAYER_HAND) == false && busted?(DEALER_HAND) == false
+    compare_hands(PLAYER_HAND, DEALER_HAND)
+  end
   prompt("Play again? (y/n)")
   answer = gets.chomp
   break unless answer.downcase == "y"
-  
   PLAYER_HAND = []
   DEALER_HAND = []
 end
